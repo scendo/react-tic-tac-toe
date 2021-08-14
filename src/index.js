64fb82bcfcf57ff3c95db7dff652714769030026
scendo <scendodusk@gmail.com>
2021-08-14 12:51:41 -0700

    MOVE: component App logic to a new component Game
    We want to separate the app wide logic/container from the game itself

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
