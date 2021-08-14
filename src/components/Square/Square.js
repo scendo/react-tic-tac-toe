2e339d22ae670f725827c013964ae8e314513586
scendo <scendodusk@gmail.com>
2021-08-14 14:10:02 -0700

    ADD: propType declarations to component Square

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Icon } from "semantic-ui-react";
 import { SquareStyles } from "./styles";
 
@@ -19,6 +20,25 @@ Square.defaultProps = {
   player: null,
 
   color: "#000",
+};
+
+Square.propTypes = {
+  /**
+   * null = no player
+   * 1 = X
+   * 2 = O
+   */
+  player: PropTypes.oneOf([null, 1, 2]),
+
+  /**
+   * the hex color value of the square
+   */
+  color: PropTypes.string,
+
+  /**
+   * callback on click
+   */
+  onClick: PropTypes.func,
 };
 
 export default Square;
